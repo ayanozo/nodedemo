40c1919e7d9f3e76e877789089cb771fc4ccf0e6
ayanozo <ayanozo@me.com>
2014-03-22 11:48:59 +0900

    update

--- app.js
+++ app.js
@@ -69,14 +69,14 @@ io.sockets.on('connection', function(socket) {
 	socket.on('message', function(data) {
 		// つながっているクライアント全員に送信
 		console.log("message:" + data);
-		io.sockets.emit('message', data);
+		socket.emit('message', data);
 	});
 	// ループテストメッセージを受けたときの処理
 	socket.on('looptest', function(data) {
 		// つながっているクライアント全員に送信
 		console.log("looptest:" + data);
 		data++;
-		io.sockets.emit('looptest', data);
+		socket.emit('looptest', data);
 	});
 	
 	// クライアントが切断したときの処理
